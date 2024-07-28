f836e2280abd5d958e6b69009afcb95aa5cebebd
burak <byrkbrk@gmail.com>
2024-07-28 17:53:33 +0300

    correct small mistakes in StableDiffusion class

--- stable_diffusion_xl_lightning.py
+++ stable_diffusion_xl_lightning.py
@@ -8,22 +8,23 @@ from safetensors.torch import load_file
 
 class StableDiffusion(object):
     def __init__(self,
-                 base: str="",
-                 repo: str="",
-                 ckpt: str="",
+                 base: str="stabilityai/stable-diffusion-xl-base-1.0",
+                 repo: str="ByteDance/SDXL-Lightning",
+                 ckpt: str="sdxl_lightning_4step_unet.safetensors",
+                 scheduler_name: str="euler_discrete_scheduler",
                  device: str=None,
                  create_dirs: bool=True
                  ):
         self.module_dir = os.path.dirname(__file__)
         self.device = self.initialize_device(device)
-        self.pipeline = ""
+        self.pipeline = self.instantiate_pipeline(base, repo, ckpt, scheduler_name, self.device)
         if create_dirs: self.create_dirs(self.module_dir)
         
     def generate(self):
         """Returns generated image for given text prompt"""
         pass
 
-    def instantiate_pipeline(self, base, repo, checkpoint, device, scheduler_name):
+    def instantiate_pipeline(self, base, repo, checkpoint, scheduler_name, device):
         """Returns instantiated pipeline"""
         pipeline = StableDiffusionXLPipeline.from_pretrained(
             base,
@@ -36,7 +37,7 @@ class StableDiffusion(object):
     def _instantiate_unet(self, base, repo, checkpoint, device):
         """Returns instantiated UNet"""
         unet = UNet2DConditionModel.from_config(base, subfolder="unet").to(device, torch.float16)
-        unet.load_state_dict(load_file(hf_hub_download(repo, checkpoint)), device=device)
+        unet.load_state_dict(load_file(hf_hub_download(repo, checkpoint), device=device))
         return unet
 
     def _set_scheduler(self, pipeline, scheduler_name):
@@ -57,4 +58,6 @@ class StableDiffusion(object):
         for dir_name in dir_names:
             os.makedirs(os.path.join(root, dir_name), exist_ok=True)
     
-        
+
+if __name__ == "__main__":
+    StableDiffusion()
