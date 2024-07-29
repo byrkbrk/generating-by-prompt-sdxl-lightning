94e7bbccdd3761cf0186fee73fc941b0bce634c8
burak <byrkbrk@gmail.com>
2024-07-29 17:58:25 +0300

    initialize generate.py file

---
+++ generate.py
@@ -0,0 +1,12 @@
+from argparse import ArgumentParser
+from stable_diffusion_xl_lightning import StableDiffusion
+
+
+
+def parse_arguments():
+    """Returns parsed arguments"""
+    pass
+
+
+if __name__ == "__main__":
+    pass
