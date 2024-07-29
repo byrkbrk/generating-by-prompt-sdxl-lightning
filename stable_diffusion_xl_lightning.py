f867a7252f96b386a479aefe4d74a72afa4d2a92
burak <byrkbrk@gmail.com>
2024-07-29 17:03:29 +0300

    add initialize_num_inference_steps method

--- stable_diffusion_xl_lightning.py
+++ stable_diffusion_xl_lightning.py
@@ -10,19 +10,27 @@ class StableDiffusion(object):
     def __init__(self,
                  base: str="stabilityai/stable-diffusion-xl-base-1.0",
                  repo: str="ByteDance/SDXL-Lightning",
-                 ckpt: str="sdxl_lightning_4step_unet.safetensors",
+                 step_choice: str="2-step",
+                 ckpt: str="sdxl_lightning_2step_unet.safetensors",
                  scheduler_name: str="euler_discrete_scheduler",
                  device: str=None,
                  create_dirs: bool=True
                  ):
+        self.step_choice = step_choice
         self.module_dir = os.path.dirname(__file__)
         self.device = self.initialize_device(device)
         self.pipeline = self.instantiate_pipeline(base, repo, ckpt, scheduler_name, self.device)
         if create_dirs: self.create_dirs(self.module_dir)
         
-    def generate(self):
+    def generate(self, prompt, show=True, save=True):
         """Returns generated image for given text prompt"""
-        pass
+        images = self.pipeline(prompt, num_inference_steps=2, guidance_scale=0).images
+        for i, image in enumerate(images):
+            if save: 
+                image.save(os.path.join(self.module_dir, "generated-images", f"generated_image_{i}.jpg"))
+            if show:
+                image.show()
+        return images
 
     def instantiate_pipeline(self, base, repo, checkpoint, scheduler_name, device):
         """Returns instantiated pipeline"""
@@ -37,7 +45,8 @@ class StableDiffusion(object):
     def _instantiate_unet(self, base, repo, checkpoint, device):
         """Returns instantiated UNet"""
         unet = UNet2DConditionModel.from_config(base, subfolder="unet").to(device, torch.float16)
-        unet.load_state_dict(load_file(hf_hub_download(repo, checkpoint), device=device))
+        unet.load_state_dict(load_file(hf_hub_download(repo, checkpoint), 
+                                       device=device.type))
         return unet
 
     def _set_scheduler(self, pipeline, scheduler_name):
@@ -50,7 +59,14 @@ class StableDiffusion(object):
 
     def initialize_device(self, device):
         """Returns the device based on GPU availability"""
-        pass
+        if device is None:
+            if torch.cuda.is_available():
+                device = "cuda"
+            elif torch.backends.mps.is_available():
+                device = "mps"
+            else:
+                device = "cpu"
+        return torch.device(device)
 
     def create_dirs(self, root):
         """Creates directories under 'root' directory required during inference"""
@@ -58,6 +74,20 @@ class StableDiffusion(object):
         for dir_name in dir_names:
             os.makedirs(os.path.join(root, dir_name), exist_ok=True)
     
+    def initialize_num_inference_steps(self, step_choice):
+        """Returns number of inference steps based on step choice"""
+        if step_choice == "1-step":
+            return 1
+        elif step_choice == "2-step":
+            return 2
+        elif step_choice == "4-step":
+            return 4
+        elif step_choice == "8-step":
+            return 8
+        else:
+            raise ValueError(f"Unexpected step choice: {step_choice}")
+
 
 if __name__ == "__main__":
-    StableDiffusion()
+    prompt = ["an image of a turtle in Picasso style"]
+    StableDiffusion().generate(prompt)
